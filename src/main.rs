0925ec88308822c4b784e85d1195d4247f0f35d5
Jack Hanslope <jackhansp@btinternet.com>
2020-04-12 13:47:48 +0100

    add enum Suit add enum Rank add struct Card

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
 fn main() {
-    println!("Hello, world!");
+    enum Suit {
+        Club,
+        Spade,
+        Heart,
+        Diamond,
+    }
+
+    enum Rank {
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Ace,
+        Queen,
+        Kind,
+        Jack,
+    }
+
+    struct Card {
+        suit: Suit,
+        rank: Rank,
+    }
 }
