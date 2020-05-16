52693ffd0d006bfa046a9e9053409594e8b31c4e
Jack Hanslope <jackhansp@btinternet.com>
2020-05-16 12:48:50 +0100

    Add round struct. Update game to use deck struct. Add function for new game. Add function for new player.

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,37 @@
 use crate::card::{Card, Suit};
+use crate::deck;
+use crate::deck::Deck;
 use serde::{Deserialize, Serialize};
 
 struct Game {
+    player1: Player,
+    player2: Player,
+    player3: Player,
+    deck: Deck,
+}
+
+impl Game {
+    fn new() -> Game {
+        let player1 = Player::new();
+        let player2 = Player::new();
+        let player3 = Player::new();
+        let deck = Deck::new();
+
+        Game {
+            player1: player1,
+            player2: player2,
+            player3: player3,
+            deck: deck,
+        }
+    }
+}
+
+// would probably be called a hand in real life, but might want to use the word hand for the set of
+// cards a player holds
+struct Round {
     state: State,
     mode: Mode,
-    deck: Vec<Card>,
+    deck: Deck,
     forehand: Player,
     middlehand: Player,
     rearhand: Player,
@@ -14,6 +41,15 @@ struct Game {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Player {
     pub hand: Vec<Card>,
+}
+
+impl Player {
+    // init a player with an empty hand
+    pub fn new() -> Player {
+        let hand: Vec<Card> = Vec::new();
+
+        Player { hand: hand }
+    }
 }
 
 enum Mode {
